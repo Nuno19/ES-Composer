fb6ee1e6faf77bddc22520ec056cc070d97a64a5
Nuno Capela <capela.nuno@ua.pt>
2019-11-03 00:21:20 +0000

    Added admin page to add movies to service

--- server.py
+++ server.py
@@ -20,16 +20,31 @@ def uploadFile():
 
         text = requests.post("http://127.0.0.1:8080/Nuno19/Recomendation_Service/1.0.0/loadItem",data={"itemList":values})
     
-        return render_template("index.html", message=text.text)
+        return render_template("admin.html", message=text.text)
 
-    return render_template("index.html", message="FAILED")
+    return render_template("admin.html", message="FAILED")
+
+@app.route('/admin')
+def getAdmin():
+    return render_template("admin.html")
+
+
+@app.route('/setk', methods = ['POST'])
+def setKandUpdate():
+    if request.method == 'POST':
+        k = request.form['k']
+        text = requests.post("http://127.0.0.1:8080/Nuno19/Recomendation_Service/1.0.0/setClusterNumber",data={"k":k})
+        update = requests.post("http://127.0.0.1:8080/Nuno19/Recomendation_Service/1.0.0/loadItemList")
+        print(text.text)
+        print(update.text)
+    return render_template("admin.html")
 
 
 @app.route('/recomended',methods=['GET'])
 def getRecomended():
     if request.method == 'GET':
-        centID = request.args.get("centID")
-        text = requests.get("http://127.0.0.1:8080/Nuno19/Recomendation_Service/1.0.0/getRecommended",{"centID":centID})
+       # centID = request.args.get("centID")
+        text = requests.get("http://127.0.0.1:8080/Nuno19/Recomendation_Service/1.0.0/getRecommended",{"centID":0})
         files = json.loads(text.text)
         dataList = [json.loads(f["data"]) for f in files]
         shuffle(dataList)
@@ -42,6 +57,8 @@ def getRecomended():
         print(files[0])
 
         return render_template("index.html", list=[dataList[:5],dataList1[:5]])
+
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
