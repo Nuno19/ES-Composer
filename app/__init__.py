16b23ff0259975570995fb361231d92032bc3564
Nuno Capela <capela.nuno@ua.pt>
2019-11-13 22:03:24 +0000

    Added option to upload files recomendation service

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,3 +10,13 @@ db = SQLAlchemy(app)
 
 from app import routes,models
 db.create_all()
+
+if app.config["UPLOAD_TO_RECOMENDATION"]:
+    print("Uploading to Recomendation Service")
+    clear = requests.post(app.config["BASE_REC_URL"]+"ClearItems")
+    print(clear.text)
+    file = open("final_metadata.tsv")
+    send = requests.post(app.config["BASE_REC_URL"]+"loadItem",{"itemList":file.read()})
+    print(send.text)
+    update = requests.post(app.config["BASE_REC_URL"]+"computeKMeans")
+    print(update.text)
